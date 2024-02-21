b8ac214d81435e4c9c363e9368d5e1884912062b
Rishi004 <rishireiko@gmail.com>
2024-02-22 01:36:15 +0530

    Netflix@Rishi completed

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "a11ecc09e77540e52497df4f205411e3";
+
+const requests = {
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+};
+
+export default requests;
